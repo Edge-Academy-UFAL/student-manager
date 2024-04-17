3a90e5c1ecd8efbe6ff33db1ead9dbcd039aeebd
Filipe <filipeflicks@gmail.com>
2024-04-17 16:34:57 -0300

    Feat: added new fields to invitation request DTO

--- src/main/java/com/academy/edge/studentmanager/dtos/InvitationRequestDTO.java
+++ src/main/java/com/academy/edge/studentmanager/dtos/InvitationRequestDTO.java
@@ -4,6 +4,7 @@ package com.academy.edge.studentmanager.dtos;
 import com.academy.edge.studentmanager.validators.EmailCollection;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -11,4 +12,8 @@ public class InvitationRequestDTO {
 
     @EmailCollection
     private List<String> emails;
+
+    private int studentGroup = 1;
+
+    private String entryDate = LocalDate.now().toString().substring(0, 8) + "01";
 }
