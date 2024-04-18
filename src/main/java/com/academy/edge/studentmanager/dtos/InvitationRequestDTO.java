4fc56eb6c081bb6c53479d885810fbf8fadaeff3
Filipe <filipeflicks@gmail.com>
2024-04-18 16:40:03 -0300

    Fix: fields studentGroup and entryDate from InvitationRequestDTO now can't be null

--- src/main/java/com/academy/edge/studentmanager/dtos/InvitationRequestDTO.java
+++ src/main/java/com/academy/edge/studentmanager/dtos/InvitationRequestDTO.java
@@ -2,6 +2,8 @@ package com.academy.edge.studentmanager.dtos;
 
 
 import com.academy.edge.studentmanager.validators.EmailCollection;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -12,7 +14,9 @@ public class InvitationRequestDTO {
     @EmailCollection
     private List<String> emails;
 
+    @NotNull
     private int studentGroup;
 
+    @NotBlank
     private String entryDate;
 }
