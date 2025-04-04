5d2e5f1213262507e699135df8c844bdb92c505f
Jeyson <jsns@ic.ufal.br>
2025-04-04 13:36:22 -0300

    feat: Config Minion

--- src/main/java/com/academy/edge/studentmanager/configs/S3Config.java
+++ src/main/java/com/academy/edge/studentmanager/configs/S3Config.java
@@ -3,13 +3,11 @@ package com.academy.edge.studentmanager.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
 
 @Configuration
 public class S3Config {
@@ -20,16 +18,20 @@ public class S3Config {
     @Value("${aws.secret.key}")
     private String awsSecretKey;
 
-    @Value("${aws.url}")
-    private String awsUrl;
+    @Value("${aws.s3.endpoint}")
+    private String awsS3Endpoint;
+
+    @Value("${aws.s3.region:us-east-1}")
+    private String region;
 
     @Bean
-    public AmazonS3 s3client(){
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey,awsSecretKey);
-        var awsS3Config = AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(new AmazonS3ClientBuilder.EndpointConfiguration(awsUrl, Regions.US_EAST_1.getName()))
+    public AmazonS3 s3client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsS3Endpoint, region))
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .enablePathStyleAccess()
                 .build();
-        return awsS3Config;
     }
 }
--- src/main/java/com/academy/edge/studentmanager/services/impl/S3ServiceImpl.java
+++ src/main/java/com/academy/edge/studentmanager/services/impl/S3ServiceImpl.java
@@ -1,40 +1,59 @@
 package com.academy.edge.studentmanager.services.impl;
 
 import com.academy.edge.studentmanager.services.S3Service;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.S3Object;
+
+import jakarta.annotation.PostConstruct;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Value;
+
 @Service
 @Log4j2
 public class S3ServiceImpl implements S3Service {
-    private AmazonS3 s3client;
+
+    private final AmazonS3 s3client;
 
     @Value("${aws.s3.bucket}")
     private String bucketName;
 
-    public S3ServiceImpl(AmazonS3 s3client, @Value("${aws.s3.bucket}") String bucketName) {
+    public S3ServiceImpl(AmazonS3 s3client) {
         this.s3client = s3client;
-        this.s3client.createBucket(bucketName);
     }
+
+    @PostConstruct
+    public void init() {
+        if (!s3client.doesBucketExistV2(bucketName)) {
+            s3client.createBucket(bucketName);
+            log.info("Bucket '{}' criado com sucesso!", bucketName);
+        } else {
+            log.info("Bucket '{}' já existe.", bucketName);
+        }
+    }
+
+    @Override
     public void uploadFile(String keyName, MultipartFile file) throws IOException {
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(file.getSize());
         metadata.setContentType(file.getContentType());
 
-        var putObjectResult = s3client.putObject(bucketName, keyName, file.getInputStream(), metadata);
-        log.info(putObjectResult.getMetadata());
-
+        s3client.putObject(bucketName, keyName, file.getInputStream(), metadata);
+        log.info("Arquivo '{}' enviado para o bucket '{}'.", keyName, bucketName);
     }
-    public S3Object getFile(String keyName){
+
+    @Override
+    public S3Object getFile(String keyName) {
+        log.info("Buscando arquivo '{}' do bucket '{}'.", keyName, bucketName);
         return s3client.getObject(bucketName, keyName);
     }
 
-    public void deleteFile(String photoUrl) {
-        s3client.deleteObject(bucketName, photoUrl);
+    @Override
+    public void deleteFile(String keyName) {
+        s3client.deleteObject(bucketName, keyName);
+        log.info("Arquivo '{}' deletado do bucket '{}'.", keyName, bucketName);
     }
 }
