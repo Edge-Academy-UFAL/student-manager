20e6b2d22ff267d8e8652b1dd28ed66482e7e176
Arthur Soares <arthurpmrs@gmail.com>
2025-05-28 10:19:25 -0300

    chore: add basic auth setup

---
+++ client/src/shared/components/providers/providers.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
+
+export function Providers({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session: Session | null;
+}) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </ThemeProvider>
+  );
+}
---
+++ client/src/shared/lib/auth.ts
@@ -0,0 +1,108 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 60 * 60 * 24, // one Day
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Sign in',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text',
+          placeholder: 'email',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const res = await fetch(
+          `${process.env.backendRoute}/api/v1/auth/login`,
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(credentials),
+          },
+        );
+
+        if (res.status === 200) {
+          const data = await res.json();
+          const user = await fetch(
+            `${process.env.backendRoute}/api/v1/auth/me`,
+            {
+              headers: {
+                Authorization: `Bearer ${data.token}`,
+              },
+            },
+          ).then((res) => res.json());
+
+          user.authToken = data.token;
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    session: ({ session, token }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          authToken: token.authToken,
+          name: token.name,
+          email: token.email,
+          photoUrl: token.photoUrl,
+          dtype: token.dtype,
+        },
+      };
+    },
+    jwt: ({ token, trigger, user, session }) => {
+      // This is necessary to update de session (and therefore, the header)
+      // when the user uploads a new profile picture
+      // More on https://next-auth.js.org/getting-started/client#updating-the-session
+      if (trigger === 'update' && session?.photoUrl) {
+        token.photoUrl = session.photoUrl;
+      }
+
+      if (user) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const u = user as unknown as any;
+
+        return {
+          ...token,
+          id: u.id,
+          authToken: u.authToken,
+          name: u.name,
+          email: u.email,
+          photoUrl: u.photoUrl,
+          dtype: u.dtype,
+        };
+      }
+      return token;
+    },
+    authorized: ({ req, token }) => {
+      if (
+        !token &&
+        !req.nextUrl.pathname.startsWith('/login') &&
+        !req.nextUrl.pathname.startsWith('/register')
+      ) {
+        return false;
+      }
+
+      return true;
+    },
+  },
+});
