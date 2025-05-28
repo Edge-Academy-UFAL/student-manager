11bfe0b18d333f42c1de1017bbdb90f9c1ff9ff2
Arthur Soares <arthurpmrs@gmail.com>
2025-05-28 09:51:31 -0300

    chore: frontend reborn

---
+++ client/next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from 'next';
+
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  env: {
+    backendRoute: process.env.backendRoute ?? 'http://127.0.0.1:8080',
+    awsUrl: process.env.awsUrl ?? 'http://172.0.0.1:4566',
+    awsBucket: process.env.awsBucket ?? 'student-manager-files',
+  },
+};
+
+export default nextConfig;
