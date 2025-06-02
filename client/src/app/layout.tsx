1ddc52be410950adb2545a79fed4d8ab03ab9d08
Arthur Soares <arthurpmrs@gmail.com>
2025-06-02 09:25:23 -0300

    feat: add variables for colors, font and radius

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Work_Sans, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/shared/components/ui/sonner';
 import { Providers } from '@/shared/components/providers/providers';
 import { auth } from '@/shared/lib/auth';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const workSans = Work_Sans({
+  variable: '--font-work-sans',
   subsets: ['latin'],
 });
 
@@ -29,7 +29,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${workSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers session={session}>{children}</Providers>
         <Toaster />
