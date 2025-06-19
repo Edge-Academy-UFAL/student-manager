45cc66fa656f035a88e02723102511e8d521f1dc
Arthur Soares <arthurpmrs@gmail.com>
2025-06-19 13:01:03 -0300

    feat: #142 - Adiciona componente de input para no de telefone com label flutuante

---
+++ client/src/shared/components/custom/floating-label-phone-input.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import PhoneInput from 'react-phone-number-input/input';
+import 'react-phone-number-input/style.css';
+
+import { FloatingLabelInput } from './floating-label-input';
+import { useId } from 'react';
+import React from 'react';
+
+type FloatingLabelPhoneInputProps = {
+  label: string;
+  value: string | undefined;
+  onChange: (value: string | undefined) => void;
+  id?: string;
+  name?: string;
+  disabled?: boolean;
+  className?: string;
+};
+
+const FloatingPhoneInputAdapter = React.forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<'input'>
+>(({ ...props }, ref) => (
+  <FloatingLabelInput label={''} {...props} ref={ref} />
+));
+FloatingPhoneInputAdapter.displayName = 'FloatingPhoneInputAdapter';
+
+export function FloatingLabelPhoneInput({
+  label,
+  value,
+  onChange,
+  id,
+  name,
+  disabled,
+  className,
+}: FloatingLabelPhoneInputProps) {
+  const generatedId = useId();
+  const inputId = id ?? generatedId;
+
+  return (
+    <div className={className}>
+      <PhoneInput
+        value={value}
+        onChange={onChange}
+        label={label}
+        inputComponent={FloatingPhoneInputAdapter}
+        id={inputId}
+        name={name}
+        disabled={disabled}
+        country="BR"
+      />
+    </div>
+  );
+}
