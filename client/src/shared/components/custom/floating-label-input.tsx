f7895ecb336197eca27df7cb7634d2a347c8d7dd
felipdsa21 <93343201+felipdsa21@users.noreply.github.com>
2025-06-30 13:12:26 -0300

    fix: #142 - Ajusta nomes das variáveis do globals.css

--- client/src/shared/components/custom/floating-label-input.tsx
+++ client/src/shared/components/custom/floating-label-input.tsx
@@ -24,7 +24,7 @@ export function FloatingLabelInput({ label, id, className, ...props }: Props) {
         htmlFor={inputId}
         data-slot="label"
         className={cn(
-          '!text-body-md peer-focus:text-action-600 peer-aria-invalid:text-danger-300 pointer-events-none absolute start-3 top-1 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-1 text-neutral-300 duration-200 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-1',
+          '!text-body-md peer-focus:text-action-950 peer-aria-invalid:text-danger-300 pointer-events-none absolute start-3 top-1 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-1 text-neutral-300 duration-200 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-1',
           'peer-disabled:text-body-md peer-disabled:peer-placeholder-shown:bg-neutral-100',
           'peer-disabled:not-peer-placeholder-shown:label-background-2-colors',
         )}
