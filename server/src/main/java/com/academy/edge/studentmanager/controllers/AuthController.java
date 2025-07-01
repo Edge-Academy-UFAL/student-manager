8073a5046b5b616af2ff5b2c001555aea0a4e842
romulo <romulosiqueira2000@gmail.com>
2025-07-01 13:05:45 -0300

    feat : #127 - Implementação do template HTML, após a solicitação para a alteração de senha.

--- server/src/main/java/com/academy/edge/studentmanager/controllers/AuthController.java
+++ server/src/main/java/com/academy/edge/studentmanager/controllers/AuthController.java
@@ -1,6 +1,5 @@
 package com.academy.edge.studentmanager.controllers;
 
-import com.academy.edge.studentmanager.dtos.AuthMeResponseDTO;
 import com.academy.edge.studentmanager.dtos.ForgetPasswordRequestDTO;
 import com.academy.edge.studentmanager.dtos.JwtAuthResponseDTO;
 import com.academy.edge.studentmanager.dtos.NewPasswordRequestDTO;
@@ -9,7 +8,6 @@ import com.academy.edge.studentmanager.models.User;
 import com.academy.edge.studentmanager.services.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -21,8 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final AuthService authService;
-    private final ModelMapper modelMapper;
+    final AuthService authService;
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @PostMapping("/login")
@@ -35,9 +32,9 @@ public class AuthController {
 
     // TODO: temporary endpoint for getting the current user
     @GetMapping("/me")
-    public ResponseEntity<AuthMeResponseDTO> me(@AuthenticationPrincipal User user) {
-        var responseDTO = modelMapper.map(user, AuthMeResponseDTO.class);
-        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    public ResponseEntity<User> me(@AuthenticationPrincipal User user) {
+        user.setPassword(null);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping("/forgot-password")
