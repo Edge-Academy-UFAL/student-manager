4c29c5b186edfbb74cbd5d69b2c404554a46e58f
felipdsa21 <93343201+felipdsa21@users.noreply.github.com>
2025-07-14 16:38:06 -0300

    refactor: #154 - Simplifica configuração do Next-Auth

--- client/src/shared/lib/auth.ts
+++ client/src/shared/lib/auth.ts
@@ -1,33 +1,23 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type User } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { getUserInfo } from './api/get-user-info-req';
 import { loginAsUser } from './api/login-as-user-req';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/login',
   },
   session: {
     strategy: 'jwt',
-    maxAge: 60 * 60 * 24, // one Day
+    maxAge: 60 * 60 * 24, // One day
   },
   providers: [
     Credentials({
-      name: 'Sign in',
       credentials: {
-        email: {
-          label: 'Email',
-          type: 'text',
-          placeholder: 'email',
-        },
+        email: { label: 'Email', type: 'email' },
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        if (!credentials?.email || !credentials.password) {
-          return null;
-        }
-
         const res = await loginAsUser({
           email: credentials.email as string,
           password: credentials.password as string,
@@ -42,39 +32,21 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     }),
   ],
   callbacks: {
-    jwt: async ({ token, trigger, user, session }) => {
-      // This is necessary to update de session (and therefore, the header)
-      // when the user uploads a new profile picture
-      // More on https://next-auth.js.org/getting-started/client#updating-the-session
-      if (trigger === 'update' && session?.photoUrl) {
-        token.photoUrl = session.photoUrl;
+    // https://github.com/nextauthjs/next-auth/blob/next-auth@5.0.0-beta.29/packages/core/src/lib/init.ts#L29
+    jwt: async ({ token, trigger, user }) => {
+      if (trigger === 'update') {
+        // Refetch user information from the backend
+        const info = await getUserInfo(token.authToken);
+        if (info) {
+          user = info as User;
+        }
       }
 
-      if (user) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const u = user as unknown as any;
-
-        return {
-          ...token,
-          id: u.id,
-          authToken: u.authToken,
-          name: u.name,
-          email: u.email,
-          photoUrl: u.photoUrl,
-          dtype: u.dtype,
-        };
-      }
-      return token;
+      return user ? { ...token, ...user } : token;
     },
     session: async ({ session, token }) => {
-      if (typeof token.id !== 'string' || typeof token.authToken !== 'string') {
-        throw new Error('Invalid token data');
-      }
-
       return {
-        ...session,
         user: {
-          ...session.user,
           id: token.id,
           authToken: token.authToken,
           name: token.name,
@@ -82,6 +54,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
           photoUrl: token.photoUrl,
           dtype: token.dtype,
         },
+        expires: session.expires?.toISOString?.() ?? session.expires,
       };
     },
   },
