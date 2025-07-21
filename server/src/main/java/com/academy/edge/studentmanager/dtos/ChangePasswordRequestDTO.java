7ff2f57b428a56d6f8c3081cbdafc38202ea79df
felipdsa21 <93343201+felipdsa21@users.noreply.github.com>
2025-07-21 11:37:30 -0300

    test #103 - Adiciona testes para trocar/esqueceu senha

--- server/src/main/java/com/academy/edge/studentmanager/dtos/ChangePasswordRequestDTO.java
+++ server/src/main/java/com/academy/edge/studentmanager/dtos/ChangePasswordRequestDTO.java
@@ -2,11 +2,13 @@ package com.academy.edge.studentmanager.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ChangePasswordRequestDTO {
     @NotBlank
     private String oldPassword;
--- server/src/main/java/com/academy/edge/studentmanager/dtos/ForgotPasswordRequestDTO.java
+++ server/src/main/java/com/academy/edge/studentmanager/dtos/ForgotPasswordRequestDTO.java
@@ -2,11 +2,13 @@ package com.academy.edge.studentmanager.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ForgotPasswordRequestDTO {
     @NotBlank
     @Email
