5281cb12332558e70f8d48641769776c70fb95d2
felipdsa21 <93343201+felipdsa21@users.noreply.github.com>
2025-08-25 16:19:36 -0300

    refactor: #98 - Simplifica `Calendar` e `FloatingLabelPhoneInput`
    Remove código repetido e/ou redundante

--- client/src/shared/components/custom/floating-label-phone-input.tsx
+++ client/src/shared/components/custom/floating-label-phone-input.tsx
@@ -1,56 +1,32 @@
 'use client';
 
-import PhoneInput from 'react-phone-number-input/input';
+import React from 'react';
+import PhoneInput, { type Props } from 'react-phone-number-input/input';
 import 'react-phone-number-input/style.css';
 
 import { FloatingLabelInput } from './floating-label-input';
-import { useId } from 'react';
-import React from 'react';
 
-type FloatingLabelPhoneInputProps = {
+interface FloatingLabelPhoneInputProps
+  extends Props<React.ComponentProps<'input'>> {
   label: string;
-  value: string | undefined;
-  onChange: (value: string | undefined) => void;
-  id?: string;
-  name?: string;
-  disabled?: boolean;
-  className?: string;
-  'aria-invalid'?: boolean;
-};
+}
 
 const FloatingPhoneInputAdapter = React.forwardRef<
   HTMLInputElement,
   React.ComponentProps<'input'>
->(({ ...props }, ref) => (
-  <FloatingLabelInput label={''} {...props} ref={ref} />
-));
+>((props, ref) => <FloatingLabelInput label="" {...props} ref={ref} />);
 FloatingPhoneInputAdapter.displayName = 'FloatingPhoneInputAdapter';
 
 export function FloatingLabelPhoneInput({
-  label,
-  value,
-  onChange,
-  id,
-  name,
-  disabled,
   className,
   ...props
 }: FloatingLabelPhoneInputProps) {
-  const generatedId = useId();
-  const inputId = id ?? generatedId;
-
   return (
     <div className={className}>
       <PhoneInput
-        value={value}
-        onChange={onChange}
-        label={label}
         inputComponent={FloatingPhoneInputAdapter}
-        id={inputId}
-        name={name}
-        disabled={disabled}
         country="BR"
-        aria-invalid={props['aria-invalid']}
+        {...props}
       />
     </div>
   );
