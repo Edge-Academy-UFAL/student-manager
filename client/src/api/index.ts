3b089943c16b35b260fad288125a4994b3564b8a
felipdsa21 <93343201+felipdsa21@users.noreply.github.com>
2025-08-25 16:25:27 -0300

    fix: #98 - Mostra mais erros de requisições

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -9,10 +9,14 @@ export function getAuthorizationHeader(session: { user: User }) {
   return { Authorization: 'Bearer ' + session.user.authToken };
 }
 
-export function throwFromResponse(
-  res: HttpResponse<unknown, { message: string }>,
+export function getErrorMessage(
+  res: HttpResponse<unknown, { message?: string; detail?: string }>,
 ) {
+  return res.error.message ?? res.error.detail;
+}
+
+export function throwFromResponse(res: Parameters<typeof getErrorMessage>[0]) {
   if (!res.ok) {
-    throw new Error(res.error.message);
+    throw new Error(getErrorMessage(res));
   }
 }
