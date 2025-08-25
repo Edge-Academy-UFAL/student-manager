e88a776ba05b30d4555ab551984b10c427c07642
felipdsa21 <93343201+felipdsa21@users.noreply.github.com>
2025-08-25 16:25:59 -0300

    refactor: #98 - Remove tela de loading

--- client/src/app/(main)/students/loading.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { DataTableSkeleton } from '@/features/student-list/components/table-skeleton';
-
-export default function Loading() {
-  return <DataTableSkeleton columnCount={4} filterableColumnCount={2} />;
-}
