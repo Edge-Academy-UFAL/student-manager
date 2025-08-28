8795eda4774536af47e0008965051978ad224642
felipdsa21 <93343201+felipdsa21@users.noreply.github.com>
2025-08-28 17:48:32 -0300

    test: #98 - Adiciona estudantes aleatórios para testes manuais

--- server/src/main/java/com/academy/edge/studentmanager/repositories/UserRepository.java
+++ server/src/main/java/com/academy/edge/studentmanager/repositories/UserRepository.java
@@ -1,11 +1,10 @@
 package com.academy.edge.studentmanager.repositories;
 
 import com.academy.edge.studentmanager.models.User;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends Repository<User, String> {
+public interface UserRepository extends CrudRepository<User, String> {
     Optional<User> findByEmail(String email);
-    void save(User user);
 }
